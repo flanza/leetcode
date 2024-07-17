20f1b7efac8102d91dc58c5060cc5d3f25da99e6
Fabio Lanza <fabio.lanza@ignitetech.com>
2024-07-17 11:12:20 -0300

    1911-maximum-alternating-subsequence-sum

--- 1911-maximum-alternating-subsequence-sum.js
+++ 1911-maximum-alternating-subsequence-sum.js
@@ -1,25 +1,45 @@
 var maxAlternatingSum = function (nums) {
-  let maxDiff = 0, max = nums[nums.length - 1];
+  return maxAlternatingSumBottomTop(nums);
+}
 
-  for (let i = 0; i < nums.length - 1; i++) {
-    let evenSum = i % 2 == 0 ? nums[i] : 0, oddSum = i % 2 != 0 ? nums[i] : 0;
-    for (let j = i + 1; j < nums.length; j++) {
-      j % 2 === 0 ? (evenSum += nums[j] ) : (oddSum += nums[j]);
-      maxDiff = Math.max(maxDiff, evenSum - oddSum);
-    }
-    max = Math.max(max,nums[i]);
+var maxAlternatingSumBottomTop = function (nums) {
+  let even = 0, odd = 0;
+  for (let i = nums.length -1; i >= 0; i--) {
+    const tmpEven = Math.max(odd + nums[i], even);
+    const tmpOdd = Math.max(even - nums[i], odd);
+    [even, odd] = [tmpEven, tmpOdd];
+  }
+  return even;
+}
+
+var maxAlternatingSumTopBottom = function (nums) {
+  const dp = {};
+
+  const dfs = (i, even) => {
+    const dpKey = even ? 'even' : 'odd';
+    if (i === nums.length) return 0;
+    if (dp[i] && dp[i][dpKey]) return dp[i][dpKey];
+
+    if (!dp[i]) dp[i] = {};
+
+    const num = even ? nums[i] : (-1 * nums[i]),
+          useThisItem = num + dfs(i + 1, !even),
+          skipThisItem = dfs(i + 1, even);
+    
+    dp[i][dpKey] = Math.max(useThisItem, skipThisItem);
+    return dp[i][dpKey];
   }
 
-  return Math.max(maxDiff, max);
-
+  const result = dfs(0, true);
+  return result;
 };
 
 const data = [
-  // { nums: [3,2,9,2,10], output: 18 },
-  // { nums: [2, 1, 5, 4, 4], output: 6 },
-  // { nums: [4, 2, 5, 3], output: 7 },
-  // { nums: [5, 6, 7, 8], output: 8 },
-  { nums: [6, 2, 1, 2, 4, 5], output: 10 }
+  { nums: [3,2,9,2,10], output: 18 },
+  { nums: [2, 1, 5, 4, 4], output: 6 },
+  { nums: [4, 2, 5, 3], output: 7 },
+  { nums: [5, 6, 7, 8], output: 8 },
+  { nums: [6,2,1,2,4,5], output: 10 }
 ];
 
 
