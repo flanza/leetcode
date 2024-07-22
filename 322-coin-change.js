1b2ecb5d9de146804f66bc7666a744d63768ffca
Fabio Lanza <fabio.lanza@ignitetech.com>
2024-07-22 10:12:47 -0300

    322-coin-change #2

--- 322-coin-change.js
+++ 322-coin-change.js
@@ -1,56 +1,57 @@
 // https://leetcode.com/problems/coin-change/description/
 
-var min = (a, b) => {
-  if (!a) return b;
-  if (!b) return a;
-  return Math.min(a, b);
+var coinChange = function (coins, amount) {
+  // const result = coinChangeTopBottom(coins, amount);
+  const result = coinChangeBottomTop(coins, amount);
+  return result;
 };
 
-var coinChange = function (coins, amount) {
-  const defaultAmount = amount + 1;
-  const dp = new Array(defaultAmount).fill(defaultAmount);
-  dp[0] = 0;
-  
-  for(let tmpAmount = 1; tmpAmount <= amount; tmpAmount++) {
-    for(const coin of coins) {
-      const prevResult = tmpAmount - parseInt(coin);
-      if (prevResult < 0) continue;
-      dp[tmpAmount] = Math.min(dp[tmpAmount], (dp[prevResult] ?? 0) + 1);
+var coinChangeBottomTop = function (coins, amount) {
+  const dp = [0];
+
+  for(let i = 1; i <= amount; i++) {
+    let minCoins = Number.MAX_SAFE_INTEGER;
+    for (const coin of coins) {
+      const coinsUsed = (dp[i - coin] ?? Number.MAX_SAFE_INTEGER) + 1;
+      minCoins = Math.min(minCoins, coinsUsed);
     }
+    dp[i] = minCoins;
   }
 
-  return dp[amount] === defaultAmount ? -1 : dp[amount]
-};
+  const result = dp[amount];
+  return result === Number.MAX_SAFE_INTEGER ? -1 : result;
+}
 
+var coinChangeTopBottom = function (coins, amount) {
+  const memo = {};
 
-// var coinChange = function (coins, amount) {
-//   const memo = {};
+  const dfs = (amountLeft) => {
+    if (memo[amountLeft]) {
+      return memo[amountLeft];
+    }
 
-//   const doChange = (coins, amount) => {
-//     if (memo[amount]) {
-//       return memo[amount];
-//     }
+    if (amountLeft === 0) {
+      return 0;
+    }
 
-//     let result;
-//     if (amount > 0) {
-//       for (let i = 0; i < coins.length; i++) {
-//         const amountLeft = amount - coins[i];
-//         if (amountLeft < 0) {
-//           continue;
-//         }
+    let minCoins = Number.MAX_SAFE_INTEGER;
+    for(let i = 0; i < coins.length; i++) {
+      const newAmount = amountLeft - coins[i];
+      if (newAmount < 0) {
+        continue;
+      }
 
-//         const answer = doChange(coins, amountLeft);
-//         if (answer !== undefined)
-//           result = min(result, answer + 1);
-//       }
-//     } else
-//       result = 0;
+      const answer = dfs(newAmount) + 1;
+      minCoins = Math.min(minCoins, answer);
+    }
 
-//     memo[amount] = result;
-//     return result;
-//   };
-//   return doChange(coins, amount) ?? -1;
-// };
+    memo[amountLeft] = Math.min(memo[amountLeft] ?? Number.MAX_SAFE_INTEGER, minCoins);
+    return minCoins;
+  }
+
+  const result = dfs(amount);
+  return result === Number.MAX_SAFE_INTEGER ? -1 : result;
+}
 
 const data = [
   {coins: [2], amount: 1, output: -1},
@@ -58,16 +59,16 @@ const data = [
   { coins: [1,2,3,4,5], amount: 7, output: 2 },
   { coins: [186, 419, 83, 408], amount: 6249, output: 20 },
   { coins: [1, 2, 5], amount: 11, output: 3 },
+  { coins: [5, 2, 1], amount: 11, output: 3 },
   { coins: [2], amount: 3, output: -1 },
   { coins: [1], amount: 0, output: 0 },
 ];
 
 
 for (let d of data) {
-
   console.log(JSON.stringify(d));
   const result = coinChange(d.coins, d.amount);
   console.log('result = ', result);
-  console.log('ok = ', result === d.output);
+  (result === d.output) ? console.log('ok') : console.error('nok');
   console.log('----------');
 }
