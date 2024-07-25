2d09b78de577d915044bdc9be75ad44ac48356e4
Fabio Lanza <fabio.lanza@ignitetech.com>
2024-07-25 10:18:12 -0300

    518-coin-change-ii #2

--- 518-coin-change-ii.js
+++ 518-coin-change-ii.js
@@ -1,35 +1,65 @@
 // https://leetcode.com/problems/coin-change-ii/description/
 
-var change = function(amount, coins) {
-  let lastRow = [], currRow = [];
-
-  for(let coinIdx = coins.length - 1; coinIdx >= 0; coinIdx--) {
-    currRow[0] = 1;
-    for(let tmpAmount = 1; tmpAmount <= amount; tmpAmount++) {
-      const prevAmount = tmpAmount - coins[coinIdx];
-      const result = (prevAmount < 0 ? 0 : currRow[prevAmount]) + (lastRow[tmpAmount] ?? 0);
-      currRow[tmpAmount] = result;
-    }
-    lastRow = currRow;
-    currRow = [];
-  }
-  return lastRow[amount];
+var change = function (amount, coins) {
+  // const result = changeTopBottom(amount, coins);
+  const result = changeBottomTop(amount, coins);
+  return result;
 };
 
+var changeBottomTop = (amount, coins) => {
+  const dp = Array.from({ length: amount + 1 }, () => 0);
+  dp[0] = 1;
 
+  for(let i = 0; i < coins.length; i++) {
+    const coin = coins[i];
+    for(let j = coin; j <= amount; j++) {
+      dp[j] += dp[j - coin];
+    }
+  }
+
+  return dp[amount];
+}
+
+var changeTopBottom = (amount, coins) => {
+  const memo = {};
+  const dfs = (i, currentAmount) => {
+    const key = `${i}-${currentAmount}`;
+    if (memo[key] !== undefined) {
+      return memo[key];
+    }
+
+    if (currentAmount === amount) {
+      return 1;
+    }
+
+    let count = 0;
+    for (let j = i; j < coins.length; j++) {
+      const newAmount = currentAmount + coins[j];
+      if (newAmount > amount) {
+        continue;
+      }
+      count += dfs(j, newAmount);
+    }
+
+    memo[key] = count;
+
+    return count;
+  };
+
+  const result = dfs(0, 0);
+  return result;
+}
 
 const data = [
-  {coins: [1,2,5], amount: 5, output: 4},
-  { coins: [3], amount: 2, output: 0 },
-  { coins: [10], amount: 10, output: 1 }
+  { coins: [1, 2, 5], amount: 5, output: 4 },
+  { coins: [2], amount: 3, output: 0 }
 ];
 
 
 for (let d of data) {
-
   console.log(JSON.stringify(d));
   const result = change(d.amount, d.coins);
   console.log('result = ', result);
-  console.log('ok = ', result === d.output);
+  (result === d.output) ? console.log('ok') : console.error('nok');
   console.log('----------');
 }
